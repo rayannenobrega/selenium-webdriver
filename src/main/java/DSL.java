e9b653f1f664bded27a50ed53fd516e66f5ebbbe
Rayanne Nóbrega <nanyz.nobrega@gmail.com>
2021-04-22 18:01:59 -0300

    organização de codigo e correção de importação

--- src/main/java/DSL.java
+++ src/main/java/DSL.java
@@ -1,12 +1,11 @@
 import java.util.ArrayList;
+import java.util.List;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
-
-import com.sun.tools.javac.util.List;
 
 public class DSL {
 
